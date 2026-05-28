04dea104820c1dee172d0a55b0e84a26a7c50230
Joseph Spann <jtspann04@gmail.com>
2026-05-27 20:16:58 -0400

    Initial commit: Eggs C# project

---
+++ Program.cs
@@ -0,0 +1,22 @@
+﻿
+using static System.Console;
+
+namespace Eggs
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int Egg1 = 30;
+            int Egg2 = 25;
+            int Egg3 = 40;
+            int Egg4 = 32;
+
+            int totalEggs = Egg1 + Egg2 + Egg3 + Egg4;
+
+            int dozens = totalEggs / 12;
+            int leftover = totalEggs % 12;
+            WriteLine("{0} eggs is {1} dozen and {2} eggs.", totalEggs, dozens, leftover);
+        }
+    }
+}
